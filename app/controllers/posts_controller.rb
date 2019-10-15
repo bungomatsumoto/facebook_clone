d0239cf877a10fb60ec16dbaf15e966d64ece5bc
bungo matsumoto <matsumotobg@gmail.com>
2019-10-15 15:58:00 +0900

    画像アップロード機能を追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -50,7 +50,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:content)
+    params.require(:post).permit(:content, :image, :image_cache)
   end
 
   def get_id_post
